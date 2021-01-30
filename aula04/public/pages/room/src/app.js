c4ee900e9ef295ab8e345102428a578d8a91cff8
raphaelvaz <raphaeltrombetta@gmail.com>
2021-01-30 13:38:24 -0300

    aula 4

---
+++ aula04/public/pages/room/src/app.js
@@ -0,0 +1,37 @@
+
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get('room');
+  console.log('this is the room', room)
+
+  //const socketUrl = 'http://localhost:3000'
+  const socketUrl = 'https://cryptic-sea-66277.herokuapp.com'
+  const socketBuilder = new SocketBuilder({ socketUrl })
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      host: 'limitless-beyond-62584.herokuapp.com',
+      secure: true,
+      //port: 9000,
+      //host: 'localhost',
+      path: '/',
+    }
+  })
+
+  const peerBuilder = new PeerBuilder({ peerConfig })
+
+  const view = new View()
+  const media = new Media()
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder,
+    peerBuilder
+  }
+
+  Business.initialize(deps);
+}
+
+window.onload = onload
